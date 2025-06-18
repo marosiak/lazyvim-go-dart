2c4c2fe70c7c986fd947c119b18e11f3808c0d79
Maciej Rosiak <root@Maciej>
2025-06-18 02:04:04 +0200

    install.sh added

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+
+read -p "🔄 Do you need backup of current config? (y/n): " backup_choice
+if [[ "$backup_choice" == "y" ]]; then
+  echo -e "${YELLOW}📦 Creating backup...${NC}"
+  mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)"
+  echo -e "${GREEN} ✅ Success!"
+fi
+
+if uname -r | grep -q "WSL"; then
+  echo -e "$BLUE Seems like you're using WSL"
+  echo -e "$GREEN it's oficially supported, in case of troubles visit README.md"
+
+fi
+
+if ! command -v git &>/dev/null; then
+  echo -e "${RED}🚨 ERROR: git is not installed!${NC}"
+  echo "🔧 Install it and try again"
+  exit 1
+else
+  git clone https://github.com/marosiak/lazyvim-go-dart ~/.config/nvim
+fi
+
+echo -e "${GREEN} 🏁 Configuration successfully installed!"
+
+if ! command -v nvim &>/dev/null; then
+  echo -e "${RED}🚨 ERROR: neovim is not installed!${NC}"
+  echo "🔧 Install it, then run 'nvim' and have fun :)"
+else
+  nvim
+fi
